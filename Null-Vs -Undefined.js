c1522328e6e3942d06cbfc24e0ee670c1d14199f
unknown <Walkershahed@gmail.com>
2021-02-02 21:58:25 -0800

    Null Vs Undefined cleard for interview

---
+++ Null-Vs -Undefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+function ADD(num1,num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add (num1 , num2){
+    console.log(num1, num2);
+}
+const premik = {nam:"shahed", phone:"932939532"};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [3,3,65,]
+console.log(ages[11]);
